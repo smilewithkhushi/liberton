ca699026e7cfbca89c5a992060b0cde16163c1d5
Khushi <exotica.xo1@gmail.com>
2023-04-24 15:01:53 +0530

    updted the dashboard page

--- app/src/main/java/com/example/studentportalloginapp/StaffDashboardActivity.java
+++ app/src/main/java/com/example/studentportalloginapp/StaffDashboardActivity.java
@@ -11,14 +11,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class dashboard extends AppCompatActivity {
+public class StaffDashboardActivity extends AppCompatActivity {
 
     TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.staff_dashboard);
         welcomeText = findViewById(R.id.welcomeMsg);
         Intent intent = getIntent();
         String name = intent.getStringExtra(MainActivity.EXTRA_NAME);
@@ -33,7 +33,7 @@ public class dashboard extends AppCompatActivity {
         });
     }
     public void logout() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(dashboard.this);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(StaffDashboardActivity.this);
         alertDialog.setTitle("Logout"); // Sets title for your alertbox
         alertDialog.setMessage("Are you sure you want to Logout ?"); // Message to be displayed on alertbox
         alertDialog.setIcon(R.drawable.logout1); // Icon for your alertbox
@@ -41,7 +41,7 @@ public class dashboard extends AppCompatActivity {
         alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 finish();
-                Toast.makeText(dashboard.this, "Successfully Logged Out", Toast.LENGTH_LONG).show();
+                Toast.makeText(StaffDashboardActivity.this, "Successfully Logged Out", Toast.LENGTH_LONG).show();
             }
         });
         alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
